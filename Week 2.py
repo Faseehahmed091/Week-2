c7a5b4f7c664f369104d33f78b7828d9766f56a7
Faseehahmed091 <146334115+Faseehahmed091@users.noreply.github.com>
2023-10-05 12:24:13 +0100

    Add files via upload
    this is my week 2 work

---
+++ Week 2.py
@@ -0,0 +1,20 @@
+total = 100
+print('The total is', total)
+total = total + 99
+print('The total is now', total)
+total += 99
+print('the total is now', total)
+total = total * 100
+print('The total is now', total)
+total *= 100
+print('The total is now', total)
+total = total - 1
+print('The total is', total)
+total = total * 4
+print('The total is', total)
+total = total/2
+print('The total is', total)
+total = 98.2
+count = 5
+average = total / count
+
